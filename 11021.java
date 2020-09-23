9d00fe9fccad83a4b748c50095e566d7b15d2351
Chae Lyn Jang <59859464+soodal5629@users.noreply.github.com>
2020-09-23 21:15:01 +0900

    add "A+B - 7"' to 11021.java of baeckjoonSolution
    This problem's title is "A+B - 7"

---
+++ 11021.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String args[]) throws IOException{
+       BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	   BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	   int t = Integer.parseInt(bf.readLine());
+	        for(int i=1;i<=t;i++){
+               String a=bf.readLine();
+	           StringTokenizer st = new StringTokenizer(a);
+	           
+	           int b=Integer.parseInt(st.nextToken());
+	           int c=Integer.parseInt(st.nextToken());
+	           
+	           bw.write("Case #"+i+": "+(b+c)+"\n");
+	        } 
+	        bf.close();
+	        bw.flush();
+    }
+}
